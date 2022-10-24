f70e850f71477cc087e15c1ea3c8584587a2fb77
keithwm422 <keithwm.422@gmail.com>
2022-10-23 21:51:18 -0400

    Populate more wires for cleaner view

--- example.py
+++ example.py
@@ -8,6 +8,23 @@ class ChargeSource:
     charge: float
     position: np.array
 
+def make_a_bunch_of_wires(which):
+      listy_objs=[]
+      if which in ['sense', 'Sense', 's', 'S', 'SENSE']:
+            for i in range(-5,5):
+                  if (i % 2) == 0: # this is even
+                        sense_wire = Circle(radius=0.1).shift(2* LEFT, i*UP)
+                  else: # this is odd
+                        sense_wire = Circle(radius=0.1).shift(2.5* LEFT, i*UP)
+                  sense_wire.set_fill(BLACK, opacity=1.0)
+                  listy_objs.append(sense_wire)
+      elif which in ['cathode', 'Cathode', 'c', 'C', 'CATHODE']:
+            for i in range(-5,5):
+                  cathode_wire = Circle(radius=0.1).shift(2 * RIGHT, i * UP)
+                  cathode_wire.set_fill(BLUE, opacity=0.5)
+                  listy_objs.append(cathode_wire)
+      return listy_objs
+
 class ElectricField:
     def __init__(self):
         self.charges = [
@@ -45,11 +62,11 @@ class Particle(Circle):
 
     def update_position(self, mobj, dt):
         # replace with if near wire...
-        if(mobj.get_x() < -10 or mobj.get_x() > 10 
-                or mobj.near_wire()):
-            mobj.v = np.zeros(2)
-            mobj.remove_updater(mobj.update_position)
-        else:
+        #if(mobj.get_x() < -10 or mobj.get_x() > 10 
+                #or mobj.near_wire()):
+            #mobj.v = np.zeros(2)
+            #mobj.remove_updater(mobj.update_position) # this stops the updating if its near a wire...
+        #else:
             jitter_probability = 0*dt
             if (np.random.uniform() < jitter_probability):
                 jitter = self.jitter_size/self.mass*np.append(np.random.uniform(low=-1,size=2))
@@ -91,37 +108,31 @@ class EventParticle(Particle):
             mobj.shift(mobj.v * dt)
 
 
-class CreateCircle(Scene):
+class CreateVideo(Scene):
     def construct(self):
-        self.e_field = ElectricField()
-        # constuct detector
-        sense_wire = Circle(radius=0.1).shift(2* LEFT)
-        sense_wire.set_fill(BLACK, opacity=1.0)
-        cathode_wire = Circle(radius=0.1).shift(2 * RIGHT)
-        cathode_wire.set_fill(BLUE, opacity=0.5) 
-
-        self.add(sense_wire)
-        self.add(cathode_wire)
-        self.wait()
-
-        # run particle event
-        be10 = EventParticle().set_z_index(3)
-        self.electrons = []
-        self.ions = []
-
-        be10.add_updater(self.update_be)
-        self.add(be10)
-        self.wait(12)
+      self.e_field = ElectricField()
+      # constuct detector
+      SenseWires=make_a_bunch_of_wires('Sense')
+      CathodeWires=make_a_bunch_of_wires('Cathode')
+      for wire in SenseWires: self.add(wire)
+      for wire in CathodeWires: self.add(wire)
+      self.wait()
+      # run particle event
+      be10 = EventParticle().set_z_index(3)
+      self.electrons = []
+      self.ions = []
+      be10.add_updater(self.update_be)
+      self.add(be10)
+      self.wait(12)
 
     def update_be(self, mobj, dt):
-        if(mobj.get_y() < -10):
+      if(mobj.get_y() < -10):
             mobj.v = np.zeros(3)
             mobj.remove_updater(mobj.update_position)
-        else:
+      else:
             a = np.zeros(3)
             mobj.v = mobj.v + a*dt 
             mobj.shift(mobj.v * dt)
-
             p_particle = 5
             prob = p_particle * dt
             if (np.random.uniform() < prob):
